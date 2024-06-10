ab3119b1e60a674c9783be359e1a370c3ee9c91f
LuisFerP12 <102192932+LuisFerP12@users.noreply.github.com>
2024-06-09 23:33:54 -0600

    dfd

---
+++ src/app/Progress/page.jsx
@@ -0,0 +1,12 @@
+//nomas es pa calar el componente pendejos de cagada
+
+import 'react-circular-progressbar/dist/styles.css';
+const TestPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      
+    </div>
+  );
+};
+
+export default TestPage;
